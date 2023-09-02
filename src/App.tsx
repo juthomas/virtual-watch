edf42ac8e632c1af79299e03cfec278638a3b5b3
juthomas <juthomas@student.42.fr>
2023-09-02 12:33:43 +0200

    [ADD] Background overflow color

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@
 // import background from './assets/clock_background_void.png'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import { Clock } from './components/Clock/Clock'
+import "./App.css";
+import { Clock } from "./components/Clock/Clock";
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <Clock/>
-    </>
-  )
+  return <Clock />;
 }
 
-export default App
+export default App;
